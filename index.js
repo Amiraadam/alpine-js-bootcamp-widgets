f1d9dd3fb4a9653cf1d451a8fd51c4a063b28874
Amira Mohammed <eng.amiraadam@gmail.com>
2024-07-16 14:24:30 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+document.addEventListener('alpine:init', () => {
+    const handler = greetHandler();
+    handler.runTests();
+});
+document.addEventListener('alpine:init', () => {
+    const handler = transportFeeHandler();
+    handler.runTests();
+});
+document.addEventListener('alpine:init', () => {
+    const handler = phoneBillHandler();
+    handler.runTests();
+});
